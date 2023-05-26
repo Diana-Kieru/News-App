2f7e57e6bb613c54df9a553e52c6d3e347929ca0
shiee <dianakieru1@gmail.com>
2023-05-26 20:50:08 +0300

    INITIAL COMMIT

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "d40226fa76394900b5d95c9e8913db3f"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+
+
+}
